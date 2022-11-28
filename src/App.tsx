f897e6243d18999342aef66627a488584e2e8a5d
annalisemshaw <sha19023@byui.edu>
2022-11-28 14:30:59 -0800

    feat(clone-page): add description paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Hello World</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
